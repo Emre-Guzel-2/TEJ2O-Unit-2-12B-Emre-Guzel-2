d5bf12355e148f25c72d01de53cd91eea3e63edd
Emre-Guzel-2 <yunus.emre.guzel@mths.ca>
2024-10-24 11:23:15 -0400

    added the sonar

--- main.ts
+++ main.ts
@@ -1,8 +1,45 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Emre Guzel
+ * Created on: Oct 24 2024
+ * This program mauseres the disance in sonar 
 */
 
-basic.showString('Hello, World!')
+// Setting the veribles 
+let distanceToObject: number = 0
+let distance = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+
+
+// Setting the program
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+// Setting ligths 
+distance.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+distance.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+distance.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+distance.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+
+// Setting the sonar 
+distanceToObject = sonar.ping(
+    DigitalPin.P1
+    ,DigitalPin.P2
+    ,PingUnit.Centimeters
+)
+
+input.onButtonPressed(Button.A,function(){
+    if (distanceToObject > 10){
+        distance.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+        distance.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
+        distance.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+        distance.setPixelColor(3, neopixel.colors(NeoPixelColors.Red))
+    }
+    else {
+        distanceToObject < 10
+        distance.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
+        distance.setPixelColor(1, neopixel.colors(NeoPixelColors.Green))
+        distance.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
+        distance.setPixelColor(3, neopixel.colors(NeoPixelColors.Green))
+
+    }
+})
