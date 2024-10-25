36a333fa21b4f01e93b0dc08c4d72a225a277641
Emre-Guzel-2 <yunus.emre.guzel@mths.ca>
2024-10-24 20:54:09 -0400

    added sonar

--- main.ts
+++ main.ts
@@ -9,7 +9,6 @@
 let distanceToObject: number = 0
 let distance = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
 
-
 // Setting the program
 basic.clearScreen()
 basic.showIcon(IconNames.Happy)
@@ -20,14 +19,16 @@ distance.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
 distance.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
 distance.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
 
-// Setting the sonar 
-distanceToObject = sonar.ping(
-    DigitalPin.P1
-    ,DigitalPin.P2
-    ,PingUnit.Centimeters
-)
+
 // When we pressed a it meauseres the current distance in cm
 input.onButtonPressed(Button.A,function(){
+    // Setting the sonar 
+    distanceToObject = sonar.ping(
+        DigitalPin.P1
+        , DigitalPin.P2
+        , PingUnit.Centimeters
+    )
+
     // If sensor is closser to something it will going to light up red neopixles.
     if (distanceToObject < 10){
         distance.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
@@ -36,9 +37,9 @@ input.onButtonPressed(Button.A,function(){
         distance.setPixelColor(3, neopixel.colors(NeoPixelColors.Red))
         distance.show()
     }
-    else {
-        //If sensor is closser to something it will going to light up Green neopixles.
-        distanceToObject >= 10
+    else
+    {
+        //If sensor is closser to something it will going to light up Green neopixles.  
         distance.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
         distance.setPixelColor(1, neopixel.colors(NeoPixelColors.Green))
         distance.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
