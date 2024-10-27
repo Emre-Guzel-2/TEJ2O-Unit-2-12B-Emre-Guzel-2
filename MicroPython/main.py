ec9f1be6e2a6d1c8e814cdc2f2268aed226a5cb8
Emre-Guzel-2 <yunus.emre.guzel@mths.ca>
2024-10-27 12:55:58 -0400

    added sonaree and the lights

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -7,22 +7,6 @@ This module is a Micro:bit MicroPython programThis program mauseres the disance 
 from microbit import *
 import neopixel
 
-# Setting the varibles 
-distanceToObject = 0
-distance2 = display.read_light_level()
-distance = neopixel.NeoPixel(pin16, 4)
-
-# Setting the screen
-display.clear()
-display.show(Image.HAPPY)
-
-# Setting up the lights 
-distance[0] =(0, 0, 0)
-distance[1] =(0, 0, 0)
-distance[2] =(0, 0, 0)
-distance[3] =(0, 0, 0)
-
-
 # Added the sonar libray
 class HCSR04:
     # this class abstracts out the functionality of the HC-SR04 and
@@ -51,16 +35,44 @@ class HCSR04:
             i = -1
         if i > 0:
             pre = bin(value).count("1")
-            # find first non full high value afterwards
-            try:
+        try:
                 k, value = next((ind, v)
                                 for ind, v in enumerate(resp[i:length - 2]) if resp[i + ind + 1] == 0)
                 post = bin(value).count("1") if k else 0
                 k = k + i
-            except StopIteration:
+        except StopIteration:
                 i = -1
         dist= -1 if i < 0 else round(((pre + (k - i) * 8. + post) * 8 * 0.172) / 2)
         return dist
 
 
-    
+# Setting the varibles 
+sonar = HCSR04
+distance = neopixel.NeoPixel(pin16, 4)
+
+# Setting the screen
+display.clear()
+display.show(Image.HAPPY)
+
+# Setting up the lights 
+distance[0] = (0, 0, 0)
+distance[1] = (0, 0, 0)
+distance[2] = (0, 0, 0)
+distance[3]  =(0, 0, 0)
+distance.show()
+while True:
+    display.show(Image.HEART)
+    display.show(sonar.distance_mm()/10)
+     
+    if button_a.is_pressed():
+        if sonar < 10:
+            distance[0] = (255, 0, 0)
+            distance[1] = (255, 0, 0)
+            distance[2] = (255, 0, 0)
+            distance[3] = (255, 0, 0)
+        else:
+            distance[0] = (255, 110, 0)
+            distance[1] = (255, 110, 0)
+            distance[2] = (255, 110, 0)
+            distance[3] = (255, 110, 0)
+        distance.show()
